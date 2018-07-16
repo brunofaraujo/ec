ad1fef8bd09ecd2a51ed5493358281ee346b1ba7
Bruno <pilinta@gmail.com>
2018-07-16 06:36:18 -0300

    Frontend improvements.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
-import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {RequestResetComponent} from './components/password/request-reset/request-reset.component';
+import {ResponseResetComponent} from './components/password/response-reset/response-reset.component';
+import {BeforeLoginService} from "./services/before-login.service";
+import {AfterLoginService} from "./services/after-login.service";
 import {
   NbAuthComponent,
   NbLoginComponent,
-  NbLogoutComponent,
   NbRegisterComponent,
+  NbLogoutComponent,
   NbRequestPasswordComponent,
   NbResetPasswordComponent,
 } from '@nebular/auth';
+import {MainComponent} from './components/main/main.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
 
-const routes: Routes = [
-  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+const appRoutes: Routes = [
   {
     path: 'auth',
     component: NbAuthComponent,
@@ -41,17 +49,53 @@ const routes: Routes = [
       },
     ],
   },
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'pages' },
-];
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [BeforeLoginService]
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    canActivate: [BeforeLoginService]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AfterLoginService]
+  },
+  {
+    path: 'request-password-reset',
+    component: RequestResetComponent,
+    canActivate: [BeforeLoginService]
+  },
+  {
+    path: 'response-password-reset',
+    component: ResponseResetComponent,
+    canActivate: [BeforeLoginService]
+  },
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+  ];
 
 const config: ExtraOptions = {
-  useHash: true,
+  useHash: false,
 };
 
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes, config)],
-  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(appRoutes, config)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,18 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './@core/utils/analytics.service';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'ngx-app',
-  template: '<router-outlet></router-outlet>',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService) {
+  constructor() {
   }
 
-  ngOnInit(): void {
-    this.analytics.trackPageViews();
+  ngOnInit() {
   }
+
 }
+
+
