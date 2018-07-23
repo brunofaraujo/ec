72ffccb87564670bfe86f679c7f91f6c93bf1b49
Bruno <pilinta@gmail.com>
2018-07-23 18:05:02 -0300

    User and Profile class.

---
+++ frontend/src/app/@core/data/profile.ts
@@ -0,0 +1,26 @@
+export class Profile {
+  id: number;
+  cpf: string;
+  user_id: number;
+  nome: string;
+  titulo: string;
+  profissao: string;
+  empresa: string;
+  genero: string;
+  nascimento: string;
+  rg_num: string;
+  rg_expeditor: string;
+  rg_estado: string;
+  cep: string;
+  logradouro: string;
+  numero: number;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  pais: string;
+  telefone: string;
+  telefone_alt: string;
+  created_at: string;
+  updated_at: string;
+}
